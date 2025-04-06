16ff4464d27808018d39ef5e6496036f4d45bdfb
Mathias Rendon <mathiassaid7@gmail.com>
2025-04-05 20:01:53 -0500

    feat: add Header component and enhance list item styling
    Introduce a new Header component with navigation links and improve the visual feedback for list items by adding a hover effect with an underline animation. This enhances the user experience and provides a more interactive navigation menu.

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+        <header className="w-full flex justify-between px-12 h-[70px] items-center bg-neutral-300">
+            <div className="header-left">
+                <h1 className="text-3xl font-bold">ToDo Api</h1>
+            </div>
+            <div className="header-right">
+                <ul className="flex justify-around gap-8">
+                    <Link to="/"><li>Inicio</li></Link>
+                    <Link to="/"><li>Tareas</li></Link>
+                    <Link to="/"><li>Contáctame</li></Link>
+                </ul>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header
